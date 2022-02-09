af8bd9a203ca4e3815710fca97b8233a1c0f31ea
Eduardo Aguilar <aheduardo5@gmail.com>
2022-02-09 17:06:06 -0600

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
-import React, { useState } from 'react';
 import './App.css';
 import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
 
 
 function App() {
-
-  const [taskList, setTasksList] = useState([]);
-  // Add Task-----------------------
-  const [task, setTask] = useState('');
-  const taskDataHandler = (e) => setTask(e.target.value);
-  
-
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault()
-    if (!task){
-      return console.log('No hay tareas')
-    }
-    const newTask = {
-      id : crypto.randomUUID(),
-      task
-    }
-    setTasksList([...taskList, newTask]);
-    setTask('')
-    
-  }
-  // End AddTask---------------------
-
-  const deleteTaskHandler = (taskId) => {
-    console.log('Id de la tarea',taskId);
-    setTasksList(taskList.filter(task => task.id !== taskId))
-  
-
-  }
-  
+ 
   return (
     <div className="App">
       <h1>Todo Manager</h1>
-      <AddTask task = {task} taskDataHandler = {taskDataHandler} onSubmitHandler = {onSubmitHandler}/>
-      <TaskList taskList={taskList} deleteTaskHandler={deleteTaskHandler}/>
+      <AddTask />
+      <TaskList />
     </div>
   );
 }
